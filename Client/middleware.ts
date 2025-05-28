61d02a9fd6e0f28320631725279ba754a7572010
Arun-DevOfficial <darunkumar1204@gmail.com>
2025-05-28 16:57:32 +0530

    Bug Fixed

--- Client/middleware.ts
+++ Client/middleware.ts
@@ -1,13 +1,18 @@
-export { withAuth as middleware } from "@kinde-oss/kinde-auth-nextjs/middleware";
+import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
+
+export default withAuth({
+  loginPage: "/api/auth/login", // Redirect unauthenticated users here
+  isReturnToProtected: true, // Redirect back to the requested page after login
+});
 
 export const config = {
   matcher: [
-    "/",
     "/design",
     "/design/view",
     "/design/view/:id*",
     "/manage",
     "/design/confirmation",
     "/design/confirmation/:id*",
+    "!/api/auth/(.*)", // Exclude KindeAuth routes
   ],
 };
