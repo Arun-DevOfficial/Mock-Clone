378e72c2ed93beeeb64fed541d7b9f5df615691c
Arun-DevOfficial <darunkumar1204@gmail.com>
2025-05-29 18:02:25 +0530

    updated middleware

--- Client/middleware.ts
+++ Client/middleware.ts
@@ -1,13 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
-import { cookies } from "next/headers";
 
 export async function middleware(req: NextRequest) {
-  const cookieStore = await cookies();
-    const isAuthenticated: string | undefined = cookieStore.get("accessToken")?.value;
-  // Todo: Validate user login
+  const isAuthenticated = req.cookies.get("accessToken")?.value;
+
   if (!isAuthenticated) {
     return NextResponse.redirect(new URL("/signin", req.url));
   }
+
   return NextResponse.next();
 }
 
