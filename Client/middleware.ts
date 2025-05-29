119922a55855e043b255fdcb78a6b04dff41fb55
Arun-DevOfficial <darunkumar1204@gmail.com>
2025-05-29 19:56:34 +0530

    update middleware

--- Client/middleware.ts
+++ Client/middleware.ts
@@ -2,7 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
   const token = req.cookies.get("accessToken")?.value;
-
+  console.log("AccessToken:", token);
+  // Todo : Validate access Token
   if (!token) {
     return NextResponse.redirect(new URL("/signin", req.url));
   }
