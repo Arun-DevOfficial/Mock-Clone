1b73fadea17f858a7c85134e4da93bf2200d7441
Arun-DevOfficial <darunkumar1204@gmail.com>
2025-05-29 18:13:23 +0530

    update middleware

---
+++ Client/middleware.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const isAuthenticated = req.cookies.get("accessToken")?.value;
+
+  if (!isAuthenticated) {
+    return NextResponse.redirect(new URL("/signin", req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    "/",
+    "/design",
+    "/design/view",
+    "/design/view/:id*",
+    "/manage",
+    "/design/confirmation",
+    "/design/confirmation/:id*",
+  ],
+};
